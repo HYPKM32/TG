d387ae62b78a191fb4ae41f118cb8d82a14a8c6c
root <root@DESKTOP-L2SC24K>
2024-11-19 14:24:44 +0900

    dockerset

---
+++ techend/src/image_detec.py
@@ -0,0 +1,5 @@
+import os
+ 
+os.system("python C:/Users/weaver/Documents/OneDrive/python/yolov5/detect.py --weights /home/app/techend/src/weights/best.pt --img 416 --conf 0.1 --source C:/Users/weaver/Documents/OneDrive/python/techno_PJ/54a60987ce1df070e41b1b7a2e7db943_t.jpeg") 
+
+
